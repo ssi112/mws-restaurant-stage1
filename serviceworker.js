d27ac250cfbc5958f9fd6daaba7d91ea4a015d73
ssi112 <brrg58@yahoo.com>
2018-05-23 20:57:49 -0400

    On branch master
    Cleaned up code formatting to comply with Udacity's specs
    
    Changes to be committed:
      modified:   index.html
      modified:   index.js
      modified:   serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -4,25 +4,25 @@
 
 var RRV_CACHE = "rest-review-cache-v1";
 var RRV_CACHE_URLS = [
-       '/',  // include the root
-       '/index.html',
-       '/restaurant.html',
-       '/index.js',
-       '/css/styles.css',
-       '/data/restaurants.json',
-       '/js/dbhelper.js',
-       '/js/main.js',
-       '/js/restaurant_info.js',
-       '/img/1.jpg',
-       '/img/2.jpg',
-       '/img/3.jpg',
-       '/img/4.jpg',
-       '/img/5.jpg',
-       '/img/6.jpg',
-       '/img/7.jpg',
-       '/img/8.jpg',
-       '/img/9.jpg',
-       '/img/10.jpg'
+  '/',  // include the root
+  '/index.html',
+  '/restaurant.html',
+  '/index.js',
+  '/css/styles.css',
+  '/data/restaurants.json',
+  '/js/dbhelper.js',
+  '/js/main.js',
+  '/js/restaurant_info.js',
+  '/img/1.jpg',
+  '/img/2.jpg',
+  '/img/3.jpg',
+  '/img/4.jpg',
+  '/img/5.jpg',
+  '/img/6.jpg',
+  '/img/7.jpg',
+  '/img/8.jpg',
+  '/img/9.jpg',
+  '/img/10.jpg'
 ];
 
 
