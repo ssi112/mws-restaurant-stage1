3855d68936f520fdc010e09c99297f49df327235
ssi112 <brrg58@yahoo.com>
2018-05-30 04:06:33 -0400

    On branch master
    Changes to be committed:
    	modified:   index.js
    
    No real change. Done as a test. Updated git for win (5/30) and received error:
      Windows git: Fatal: TaskCanceledException encountered
      Fix: git config --global credential.helper wincred

--- index.js
+++ index.js
@@ -6,8 +6,8 @@
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("/serviceworker.js")
     .then(function(registration) {
-      console.log("Service Worker registered with scope:", registration.scope);
+      console.log("Service Worker registered with scope: ", registration.scope);
     }).catch(function(err) {
-      console.log("Service Worker registration failed:", err);
+      console.log("Service Worker registration failed: ", err);
     });
 }
